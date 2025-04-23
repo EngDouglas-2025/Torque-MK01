7b09488084cff981a6cfaed7a2eca2c54461321e
EngDouglas-2025 <engmichaeldmagalhaes@gmail.com>
2025-04-23 15:08:45 -0300

    Add files via upload

---
+++ torque-calculator-app/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TorqueCalculator from './TorqueCalculator'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TorqueCalculator />
+  </React.StrictMode>
+)
